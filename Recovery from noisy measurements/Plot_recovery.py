21adb195e48aaef1d0d3aa37d9a436839e5044b3
Shay Kreymer <kreymershay@gmail.com>
2021-04-04 18:55:13 +0300

    Data for Section III-A

--- Recovery from noisy measurements/Plot_recovery.py
+++ Recovery from noisy measurements/Plot_recovery.py
@@ -9,7 +9,7 @@ Created on Wed Mar 31 14:07:04 2021
 import numpy as np
 import matplotlib.pyplot as plt
 
-from Utils.mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import scipy
 
@@ -26,11 +26,11 @@ plt.close("all")
 
 
 X = plt.imread("../images/molecule9.png")
-X = X
+X = X * 10
 L = np.shape(X)[0]
 W = L # L for arbitrary spacing distribution, 2*L-1 for well-separated
 
-ne = 40
+ne = 34
 B, z, roots, kvals, nu = expand_fb(X, ne)
 T = calcT(nu, kvals)
 BT = B @ T.H
@@ -38,8 +38,8 @@ c = np.real(T @ z)
 z = T.H@c
 Xrec_true = np.reshape(np.real(B @ z), np.shape(X))
 
-z_est_best_no_noise = np.load("../Results/Recovery/z_est_best_no_noise.npy")
-X_no_noise = np.reshape(np.real(B @ z_est_best_no_noise), np.shape(X))
+
+
 
 # %% plots
 # with plt.style.context('ieee'):
@@ -49,7 +49,7 @@ ax = plt.axes()
 im = ax.imshow(Xrec_true)
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
-im.set_clim(0, 0.7)
+im.set_clim(0, 7)
 cbar = plt.colorbar(im, cax=cax)
 cbar.ax.tick_params(labelsize=20)
 # ax.text(-0.1, 1.03, '('+string.ascii_lowercase[n]+')', transform=ax.transAxes, 
@@ -61,13 +61,17 @@ plt.show()
 plt.savefig(r'C:\Users\kreym\Google Drive\Thesis\Documents\Article\figures\recovery_true.pdf')
 
 
+
+z_est_best_no_noise = np.load("../Results/Recovery/z_est_best_no_noise.npy")
+X_no_noise = np.reshape(np.real(B @ z_est_best_no_noise), np.shape(X))
+
 fig = plt.figure()
 
 ax = plt.axes()
 im = ax.imshow(X_no_noise)
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
-im.set_clim(0, 0.7)
+im.set_clim(0, 7)
 cbar = plt.colorbar(im, cax=cax)
 cbar.ax.tick_params(labelsize=20)
 # ax.text(-0.1, 1.03, '('+string.ascii_lowercase[n]+')', transform=ax.transAxes, 
@@ -78,5 +82,25 @@ plt.show()
     
 plt.savefig(r'C:\Users\kreym\Google Drive\Thesis\Documents\Article\figures\recovery_no_noise.pdf')
 
+
+z_est_best_SNR10 = np.load("../Results/Recovery/z_est_best_SNR10.npy")
+X_SNR10 = np.reshape(np.real(B @ z_est_best_SNR10), np.shape(X))
+
+fig = plt.figure()
+
+ax = plt.axes()
+im = ax.imshow(X_SNR10)
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+im.set_clim(0, 7)
+cbar = plt.colorbar(im, cax=cax)
+cbar.ax.tick_params(labelsize=20)
+# ax.text(-0.1, 1.03, '('+string.ascii_lowercase[n]+')', transform=ax.transAxes, 
+        # size=30, weight='bold')
+ax.tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+fig.tight_layout()
+plt.show()
+    
+plt.savefig(r'C:\Users\kreym\Google Drive\Thesis\Documents\Article\figures\recovery_SNR10.pdf')
 
     
