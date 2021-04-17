19608bf79f1218c5316ff60de3c09ba58f178ecf
Shay Kreymer <kreymershay@gmail.com>
2021-04-17 16:31:03 +0300

    Modifications

--- Recovery error as a function of SNR/est_err_SNR.py
+++ Recovery error as a function of SNR/est_err_SNR.py
@@ -7,24 +7,20 @@ Created on Thu Apr  1 12:02:30 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-
 
 import multiprocessing as mp
 
 from Utils.calc_err_SNR import calc_err_SNR
 
-
 plt.close("all")
 
 if __name__ == '__main__':
-    start = time.time()
-    N = 30000
-    Niters = 20
-    SNRs_length = 25
+    N = 20000
+    Niters = 30
+    SNRs_length = 30
     L = 5
     ne = 10
-    SNRs = np.logspace(np.log10(0.5), np.log10(100), SNRs_length)
+    SNRs = np.logspace(np.log10(0.09), np.log10(100), SNRs_length)
     
     num_cpus = mp.cpu_count()
     pool = mp.Pool(num_cpus)
@@ -32,23 +28,17 @@ if __name__ == '__main__':
     pool.close()
     pool.join() 
     
-    finish = time.time() - start
-    
     errs = np.zeros((Niters, SNRs_length))
 
     for j in range(Niters):
-        errs[j, :] = np.min(S[j][0], axis=1)
+        errs[j, :] = S[j][0][np.arange(SNRs_length), np.argmin(S[j][1], axis=1)]
     errs_mean = np.median(errs, 0)
-    
-    np.save('SNRs.npy', SNRs)
-    
-    np.save('errs_SNR_0_25.npy', errs)
-
-
-    plt.figure()
-    plt.loglog(SNRs, errs_mean, '.-')  
-    plt.loglog(SNRs[-16:-10], errs_mean[-16]*(SNRs[-16:-10]/SNRs[-16])**(-1.5), '--')
-    plt.xlabel('SNR')
-    plt.ylabel('MSE')
-    # plt.legend(('data', '-1/2 slope'))
-    
+    with plt.style.context('ieee'):
+        fig = plt.figure()
+        plt.loglog(SNRs[4:18], errs_mean[4]*(SNRs[4:18]/SNRs[4])**(-3/4), 'k--', lw=0.5)
+        plt.loglog(SNRs, errs_mean, '.-b')  
+        plt.xlabel('SNR')
+        plt.ylabel('Median estimation error')
+        fig.tight_layout()
+        plt.show()
+        plt.savefig(r'C:\Users\kreym\Google Drive\Thesis\Documents\Article\figures\error_SNR_experiment.pdf')
