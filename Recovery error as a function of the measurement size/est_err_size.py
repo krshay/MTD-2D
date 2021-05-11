c64f12f2b2ba6bbce4be4a882fb2ea53a6760dd9
Shay Kreymer <kreymershay@gmail.com>
2021-05-11 20:28:38 +0300

    Error vs. size
    Updated error vs. size experiments to 10 initial guessrs.

--- Recovery error as a function of the measurement size/est_err_size.py
+++ Recovery error as a function of the measurement size/est_err_size.py
@@ -7,18 +7,18 @@ Created on Mon Nov 23 14:27:49 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stats
 import multiprocessing as mp
-from Utils.calc_err_size import calc_err_size_knownpsftsf, calc_err_size_Algorithm1, calc_err_size_nopsftsf
+from Utils.calc_err_size import calc_err_size_both, calc_err_size_nopsftsf
 
 plt.close("all")
+
 
 if __name__ == '__main__':
     # Code to reproduce Fig. 5 in the paper. 
     # Estimation error as a function of measurement size for 3 cases: known pair separation functions, Algorithm 1, and assuming the well-separated model.
     # %% Preliminary definitions
     N = 30000
-    Niters = 100
+    Niters = 10
     L = 5
     ne = 10
     Nsizes = 15
@@ -27,15 +27,21 @@ if __name__ == '__main__':
     num_cpus = mp.cpu_count()
     # %% Known psf and tsf
     pool = mp.Pool(num_cpus)
-    Sknown = pool.starmap(calc_err_size_knownpsftsf, [[L, ne, sizes, i] for i in range(Niters)])
+    S = pool.starmap(calc_err_size_both, [[L, ne, sizes, i+50] for i in range(Niters)])
     pool.close()
     pool.join() 
     
-    # %% Algorithm1
-    pool = mp.Pool(num_cpus)
-    SAlgorithm1 = pool.starmap(calc_err_size_Algorithm1, [[L, ne, sizes, i] for i in range(Niters)])
-    pool.close()
-    pool.join()
+    errs_known = np.zeros((Niters, Nsizes))
+
+    for j in range(Niters):
+        errs_known[j, :] = S[j][0][np.arange(Nsizes), np.argmin(S[j][1], axis=1)]
+    errs_known_mean = np.mean(errs_known, 0)
+    
+    errs_Algorithm1 = np.zeros((Niters, Nsizes))
+
+    for j in range(Niters):
+        errs_Algorithm1[j, :] = S[j][2][np.arange(Nsizes), np.argmin(S[j][3], axis=1)]
+    errs_Algorithm1_mean = np.mean(errs_Algorithm1, 0)
     
     # %% No psf and tsf
     sizes_no = np.logspace(np.log10(1000), np.log10(N), 5).astype(int)
@@ -45,36 +51,28 @@ if __name__ == '__main__':
     pool.join()
     
     # %% Calculations
-    errsAlgorithm1 = np.zeros((Niters, Nsizes))
-
-    for j in range(Niters):
-        errsAlgorithm1[j, :] = SAlgorithm1[j][0][np.arange(Nsizes), np.argmin(SAlgorithm1[j][1], axis=1)]
-    errsAlgorithm1_median = stats.trim_mean(errsAlgorithm1, 0.06, 0)
-    
-    errsknown = np.zeros((Niters, Nsizes))
-
-    for j in range(Niters):
-        errsknown[j, :] = Sknown[j][0][np.arange(Nsizes), np.argmin(Sknown[j][1], axis=1)]
-    errsknown_median = stats.trim_mean(errsknown, 0.06, 0)
-    
     errs_no = np.zeros((Niters, Nsizes))
 
     for j in range(Niters):
         errs_no[j, :] = Sno[j][0][np.arange(Nsizes), np.argmin(Sno[j][1], axis=1)]
-    errs_no_median = stats.trim_mean(errs_no, 0.06, 0)
-    
+    errs_no_mean = np.mean(errs_no, 0)
     # %% plots
     plt.close("all")
     with plt.style.context('ieee'):
         fig = plt.figure()
-        plt.loglog(sizes**2, errsknown_median[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
-        plt.loglog(sizes**2, errsknown_median, '.-b', label=r'known $\xi$ and $\zeta$')
-        plt.loglog(sizes**2, errsAlgorithm1_median[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
-        plt.loglog(sizes**2, errsAlgorithm1_median, '.--r', label='Algorithm 1')
-        plt.loglog(sizes_no**2, errs_no_median, ':g', label=r'no $\xi$ and $\zeta$')
-        plt.legend(loc=(0.5, 0.62))
+        
+        plt.loglog(sizes**2, errs_known_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
+        plt.loglog(sizes**2, errs_known_mean, '.-b', label=r'known $\xi$ and $\zeta$')
+    
+        plt.loglog(sizes**2, errs_Algorithm1_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
+        plt.loglog(sizes**2, errs_Algorithm1_mean, '.--r', label='Algorithm 1')
+        
+        plt.loglog(sizes_no**2, errs_no_mean, ':g', label=r'no $\xi$ and $\zeta$')
+    
+        plt.legend(loc=(0.5, 0.62))#, fontsize=6)
+        
         plt.xlabel('Measurement size [pixels]')
+        
         plt.ylabel('Mean estimation error')
         fig.tight_layout()
         plt.show()
-        
--- Utils/calc_err_size.py
+++ Utils/calc_err_size.py
@@ -12,7 +12,7 @@ from Utils.funcs_calc_moments import M2_2d, M3_2d
 from Utils.psf_tsf_funcs import full_psf_2d, full_tsf_2d, makeExtraMat, maketsfMat
 import Utils.optimization_funcs_rot
 
-def calc_err_size_knownpsftsf(L, ne, sizes, sd):
+def calc_err_size_both(L, ne, sizes, sd):
     """ Calculate estimation error in estimating a specific target image, multiple micrograph sizes. For the case of known PSF and TSF.
 
     Args:
@@ -23,12 +23,16 @@ def calc_err_size_knownpsftsf(L, ne, sizes, sd):
 
     Returns:
         errs: an array containing the estimation errors for each size (3 initial guesses)
-        costs: an array containing the objective function values for each size (3 initial guesses)
+        errs: an array containing the objective function values for each size (3 initial guesses)
     """
     # %% preliminary definitions
     np.random.seed(sd)
-    errs = np.zeros((len(sizes), 3))
-    costs = np.zeros((len(sizes), 3))
+    NumGuesses = 10
+    errs_known = np.zeros((len(sizes), NumGuesses))
+    costs_known = np.zeros((len(sizes), NumGuesses))
+    errs_Algorithm1 = np.zeros((len(sizes), NumGuesses))
+    costs_Algorithm1 = np.zeros((len(sizes), NumGuesses))
+    
     X = np.random.rand(L, L)
     X = X / np.linalg.norm(X)
     
@@ -45,27 +49,17 @@ def calc_err_size_knownpsftsf(L, ne, sizes, sd):
     # %% initial guesses
     gamma_initial = 0.09
 
-    X_initial1 = np.random.rand(L, L)
-    X_initial1 = X_initial1 / np.linalg.norm(X_initial1)
-    
-    _, z_initial1, _, _, _ = expand_fb(X_initial1, ne)
-    c_initial1 = np.real(T @ z_initial1)
-
-    X_initial2 = np.random.rand(L, L)
-    X_initial2 = X_initial2 / np.linalg.norm(X_initial2)
-    
-    _, z_initial2, _, _, _ = expand_fb(X_initial2, ne)
-    c_initial2 = np.real(T @ z_initial2)
-    
-    X_initial3 = np.random.rand(L, L)
-    X_initial3 = X_initial3 / np.linalg.norm(X_initial3)
-    
-    _, z_initial3, _, _, _ = expand_fb(X_initial3, ne)
-    c_initial3 = np.real(T @ z_initial3)
+    cs = np.zeros((NumGuesses, ne))
+    for jj in range(NumGuesses):
+        X_initial = np.random.rand(L, L)
+        X_initial = X_initial / np.linalg.norm(X_initial)
+        _, z_initial, _, _, _ = expand_fb(X_initial, ne)
+        c_initial = np.real(T @ z_initial)
+        cs[jj, :] = c_initial
 
     # %% calculations
     for (idx, sz) in enumerate(sizes):
-        print(f'{sz} * {sz} in seed #{sd}')
+
         y_clean, s, locs = generate_clean_micrograph_2d_rots(c, kvals, Bk, W, L, sz, 0.100*(sz/L)**2, T, seed=sd)
         sigma2 = 0
         y = y_clean + np.random.default_rng().normal(loc=0, scale=np.sqrt(sigma2), size=np.shape(y_clean))
@@ -92,128 +86,28 @@ def calc_err_size_knownpsftsf(L, ne, sizes, sd):
                     for j2 in range(L):
                         M3_y[i1, j1, i2, j2] = M3_2d(yy, (i1, j1), (i2, j2))
         del yy
-        X_est1 = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial1)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
-        c_est1 = X_est1.x[1:]
-        z_est1 = T.H @ c_est1
-        est_err_coeffs1 = min_err_coeffs(z, z_est1, kvals)
-        errs[idx, 0] = est_err_coeffs1[0]
-        costs[idx, 0] = X_est1.fun
+        np.save(f'M1_y_size_{sz}_seed_{sd}.npy', M1_y)
+        np.save(f'M2_y_size_{sz}_seed_{sd}.npy', M2_y)
+        np.save(f'M3_y_size_{sz}_seed_{sd}.npy', M3_y)
         
-        X_est2 = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial2)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
-        c_est2 = X_est2.x[1:]
-        z_est2 = T.H @ c_est2
-        est_err_coeffs2 = min_err_coeffs(z, z_est2, kvals)
-        errs[idx, 1] = est_err_coeffs2[0]
-        costs[idx, 1] = X_est2.fun
-        
-        X_est3 = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial3)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
-        c_est3 = X_est3.x[1:]
-        z_est3 = T.H @ c_est3
-        est_err_coeffs3 = min_err_coeffs(z, z_est3, kvals)
-        errs[idx, 2] = est_err_coeffs3[0]
-        costs[idx, 2] = X_est3.fun
-        
-    return errs, costs
+        for jj in range(NumGuesses):
+            X_est_known = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), cs[jj, :])), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
+            c_est_known = X_est_known.x[1:]
+            z_est_known = T.H @ c_est_known
+            est_err_coeffs_known = min_err_coeffs(z, z_est_known, kvals)
+            errs_known[idx, jj] = est_err_coeffs_known[0]
+            costs_known[idx, jj] = X_est_known.fun
+            
+            X_est_Algorithm1, _, _ = Utils.optimization_funcs_rot.optimize_rot_Algorithm1_notparallel(np.concatenate((np.reshape(gamma_initial, (1,)), cs[jj, :])), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, W, 10000) 
+            c_est_Algorithm1 = X_est_Algorithm1.x[1:]
+            z_est_Algorithm1 = T.H @ c_est_Algorithm1
+            est_err_coeffs_Algorithm1 = min_err_coeffs(z, z_est_Algorithm1, kvals)
+            errs_Algorithm1[idx, jj] = est_err_coeffs_Algorithm1[0]
+            costs_Algorithm1[idx, jj] = X_est_Algorithm1.fun
+            
+        print(f'finished {sz} * {sz} in seed #{sd}')
+    return errs_known, costs_known, errs_Algorithm1, costs_Algorithm1
 
-def calc_err_size_Algorithm1(L, ne, sizes, sd):
-    """ Calculate estimation error in estimating a specific target image, multiple micrograph sizes. For the case of Algorithm1.
-
-    Args:
-        L: diameter of the target image
-        ne: number of expansion coefficients
-        sizes: an array containing the desired values of N, the size of the micrographs to be generated
-        sd: a seed
-
-    Returns:
-        errs: an array containing the estimation errors for each size (3 initial guesses)
-        costs: an array containing the objective function values for each size (3 initial guesses)
-    """
-    # %% preliminary definitions
-    np.random.seed(sd)
-    errs = np.zeros((len(sizes), 3))
-    costs = np.zeros((len(sizes), 3))
-    X = np.random.rand(L, L)
-    X = X / np.linalg.norm(X)
-    
-    W = L # L for arbitrary spacing distribution, 2*L-1 for well-separated
-
-    B, z, roots, kvals, nu = expand_fb(X, ne)
-    T = calcT(nu, kvals)
-    c = np.real(T @ z)
-    z = T.H@c
-    Bk = np.zeros((2*L-1, 2*L-1, nu), dtype=np.complex_)
-    for ii in range(nu):
-        Bk[ :, :, ii] = np.fft.fft2(np.pad(np.reshape(B[ :, ii], (L, L)), L//2))
-
-    # %% initial guesses
-    gamma_initial = 0.09
-
-    X_initial1 = np.random.rand(L, L)
-    X_initial1 = X_initial1 / np.linalg.norm(X_initial1)
-    
-    _, z_initial1, _, _, _ = expand_fb(X_initial1, ne)
-    c_initial1 = np.real(T @ z_initial1)
-
-    X_initial2 = np.random.rand(L, L)
-    X_initial2 = X_initial2 / np.linalg.norm(X_initial2)
-    
-    _, z_initial2, _, _, _ = expand_fb(X_initial2, ne)
-    c_initial2 = np.real(T @ z_initial2)
-    
-    X_initial3 = np.random.rand(L, L)
-    X_initial3 = X_initial3 / np.linalg.norm(X_initial3)
-    
-    _, z_initial3, _, _, _ = expand_fb(X_initial3, ne)
-    c_initial3 = np.real(T @ z_initial3)
-    
-    # %% calculations
-    for (idx, sz) in enumerate(sizes):
-        print(f'{sz} * {sz} in seed #{sd}')
-        y_clean, s, locs = generate_clean_micrograph_2d_rots(c, kvals, Bk, W, L, sz, 0.100*(sz/L)**2, T, seed=sd)
-        sigma2 = 0
-        y = y_clean + np.random.default_rng().normal(loc=0, scale=np.sqrt(sigma2), size=np.shape(y_clean))
-        del y_clean
-        yy = np.zeros((sz, sz, 1))
-        yy[ :, :, 0] = y
-        del y
-        
-        M1_y = np.mean(yy)
-        
-        M2_y = np.zeros((L, L))
-        for i1 in range(L):
-            for j1 in range(L):
-                M2_y[i1, j1] = M2_2d(yy, (i1, j1))
-        
-        M3_y = np.zeros((L, L, L, L))
-        for i1 in range(L):
-            for j1 in range(L):
-                for i2 in range(L):
-                    for j2 in range(L):
-                        M3_y[i1, j1, i2, j2] = M3_2d(yy, (i1, j1), (i2, j2))
-        del yy
-        X_est1, _, _ = Utils.optimization_funcs_rot.optimize_rot_Algorithm1_notparallel(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial1)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, W, 10000) 
-        X_est2, _, _ = Utils.optimization_funcs_rot.optimize_rot_Algorithm1_notparallel(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial2)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, W, 10000) 
-        X_est3, _, _ = Utils.optimization_funcs_rot.optimize_rot_Algorithm1_notparallel(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial3)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, W, 10000) 
-
-        c_est1 = X_est1.x[1:]
-        z_est1 = T.H @ c_est1
-        est_err_coeffs1 = min_err_coeffs(z, z_est1, kvals)
-        errs[idx, 0] = est_err_coeffs1[0]
-        costs[idx, 0] = X_est1.fun
-        
-        c_est2 = X_est2.x[1:]
-        z_est2 = T.H @ c_est2
-        est_err_coeffs2 = min_err_coeffs(z, z_est2, kvals)
-        errs[idx, 1] = est_err_coeffs2[0]
-        costs[idx, 1] = X_est2.fun
-        
-        c_est3 = X_est3.x[1:]
-        z_est3 = T.H @ c_est3
-        est_err_coeffs3 = min_err_coeffs(z, z_est3, kvals)
-        errs[idx, 2] = est_err_coeffs3[0]
-        costs[idx, 2] = X_est3.fun
-        
-    return errs, costs
 
 def calc_err_size_nopsftsf(L, ne, sizes, sd):
     """ Calculate estimation error in estimating a specific target image, multiple micrograph sizes. For the case of assuming the well-separated model.
@@ -226,13 +120,13 @@ def calc_err_size_nopsftsf(L, ne, sizes, sd):
 
     Returns:
         errs: an array containing the estimation errors for each size (3 initial guesses)
-        costs: an array containing the objective function values for each size (3 initial guesses)
+        errs: an array containing the objective function values for each size (3 initial guesses)
     """
     # %% preliminary definitions
     np.random.seed(sd)
-    errs = np.zeros((len(sizes), 3))
-    costs = np.zeros((len(sizes), 3))
-    X_ests = np.zeros((len(sizes), 3, ne+1))
+    NumGuesses = 10
+    errs = np.zeros((len(sizes), NumGuesses))
+    costs = np.zeros((len(sizes), NumGuesses))
     X = np.random.rand(L, L)
     X = X / np.linalg.norm(X)
     
@@ -250,27 +144,16 @@ def calc_err_size_nopsftsf(L, ne, sizes, sd):
     gamma_initial = 0.09
     # y_init, s_init, locs_init = generate_clean_micrograph_2d_rots(c, kvals, Bk, W, L, N, gamma*(N/L)**2, T)
    
-    X_initial1 = np.random.rand(L, L)
-    X_initial1 = X_initial1 / np.linalg.norm(X_initial1)
-    
-    _, z_initial1, _, _, _ = expand_fb(X_initial1, ne)
-    c_initial1 = np.real(T @ z_initial1)
-
-    X_initial2 = np.random.rand(L, L)
-    X_initial2 = X_initial2 / np.linalg.norm(X_initial2)
-    
-    _, z_initial2, _, _, _ = expand_fb(X_initial2, ne)
-    c_initial2 = np.real(T @ z_initial2)
-    
-    X_initial3 = np.random.rand(L, L)
-    X_initial3 = X_initial3 / np.linalg.norm(X_initial3)
-    
-    _, z_initial3, _, _, _ = expand_fb(X_initial3, ne)
-    c_initial3 = np.real(T @ z_initial3)
+    cs = np.zeros((NumGuesses, ne))
+    for jj in range(NumGuesses):
+        X_initial = np.random.rand(L, L)
+        X_initial = X_initial / np.linalg.norm(X_initial)
+        _, z_initial, _, _, _ = expand_fb(X_initial, ne)
+        c_initial = np.real(T @ z_initial)
+        cs[jj, :] = c_initial
 
     # %% calculations
     for (idx, sz) in enumerate(sizes):
-        print(f'{sz} * {sz} in seed #{sd}')
         y_clean, s, locs = generate_clean_micrograph_2d_rots(c, kvals, Bk, W, L, sz, 0.100*(sz/L)**2, T, seed=sd)
         # gamma = s[0]*(L/N)**2
         sigma2 = 0
@@ -298,29 +181,14 @@ def calc_err_size_nopsftsf(L, ne, sizes, sd):
                     for j2 in range(L):
                         M3_y[i1, j1, i2, j2] = M3_2d(yy, (i1, j1), (i2, j2))
     
-        X_est1 = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial1)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
-        c_est1 = X_est1.x[1:]
-        z_est1 = T.H @ c_est1
-        est_err_coeffs1 = min_err_coeffs(z, z_est1, kvals)
-        errs[idx, 0] = est_err_coeffs1[0]
-        costs[idx, 0] = X_est1.fun
-        X_ests[idx, 0, :] = X_est1.x
+        for jj in range(NumGuesses):
+            X_est_known = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), cs[jj, :])), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
+            c_est_known = X_est_known.x[1:]
+            z_est_known = T.H @ c_est_known
+            est_err_coeffs_known = min_err_coeffs(z, z_est_known, kvals)
+            errs[idx, jj] = est_err_coeffs_known[0]
+            costs[idx, jj] = X_est_known.fun
+        print(f'finished {sz} * {sz} in seed #{sd}')
         
-        X_est2 = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial2)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
-        c_est2 = X_est2.x[1:]
-        z_est2 = T.H @ c_est2
-        est_err_coeffs2 = min_err_coeffs(z, z_est2, kvals)
-        errs[idx, 1] = est_err_coeffs2[0]
-        costs[idx, 1] = X_est2.fun
-        X_ests[idx, 1, :] = X_est2.x
-        
-        X_est3 = Utils.optimization_funcs_rot.optimize_2d_known_psf_triplets(np.concatenate((np.reshape(gamma_initial, (1,)), c_initial3)), Bk, T, kvals, M1_y, M2_y, M3_y, sigma2, L, 1, tsfMat, ExtraMat2, ExtraMat3) 
-        c_est3 = X_est3.x[1:]
-        z_est3 = T.H @ c_est3
-        est_err_coeffs3 = min_err_coeffs(z, z_est3, kvals)
-        errs[idx, 2] = est_err_coeffs3[0]
-        costs[idx, 2] = X_est3.fun
-        X_ests[idx, 2, :] = X_est3.x
-        
-    return errs, costs, X_ests, c
+    return errs, costs
     
